236b6a612980182b1844d2ddd38ec9a1f9b14c14
antares-v <175059906+antares-v@users.noreply.github.com>
2025-10-22 17:28:58 -0500

    Initial Commit

---
+++ app-accelerator-2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app-accelerator-2025
+//
+//  Created by 33 GO Participant on 10/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app-accelerator-2025/app_accelerator_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  app_accelerator_2025App.swift
+//  app-accelerator-2025
+//
+//  Created by 33 GO Participant on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct app_accelerator_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
